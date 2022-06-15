c6038b9140f5ff5098ab689032afeb8b3affeac3
WendyHM <wendy.hm005@ciencias.unam.mx>
2022-06-14 20:44:37 -0500

    Configuración de Registro y Login

--- ProyectoDjango/proyectoDIU/proyectoDIU/settings.py
+++ ProyectoDjango/proyectoDIU/proyectoDIU/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
@@ -121,9 +121,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGIN_REDIRECT_URL = 'subirarchivos'
+
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
