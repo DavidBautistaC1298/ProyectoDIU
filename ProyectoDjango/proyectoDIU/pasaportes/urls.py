e21bb8fad71ffd3b34140ecf5f90bf3b1cfa78d9
Paola Barredo <paolabarredo@ciencias.unam.mx>
2022-06-15 00:20:20 -0500

    Url pago
    Se agrega url de pago

--- ProyectoDjango/proyectoDIU/pasaportes/urls.py
+++ ProyectoDjango/proyectoDIU/pasaportes/urls.py
@@ -9,5 +9,6 @@ path('login', views.login, name='login'),
 path('tramitepresencial', views.tramitepresencial, name='tramitepresencial'),
 path('subirarchivos', views.subirarchivos, name='subirarchivos'),
 path('tramitenacional', views.tramitenacional, name='tramitenacional'),
-path('tramiteinternacional', views.tramiteinternacional, name='tramiteinternacional')
+path('tramiteinternacional', views.tramiteinternacional, name='tramiteinternacional'),
+path('pago',views.pago,name='pago')
 ]
